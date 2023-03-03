52552f245d00d62f4518297bed3069e8381b1bac
keylorjc1 <keylorjc1@hotmail.com>
2023-03-02 18:25:42 -0600

    Edit y remove creado

--- src/main/java/com/tienda/controller/PersonaController.java
+++ src/main/java/com/tienda/controller/PersonaController.java
@@ -15,6 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -43,6 +46,27 @@ public class PersonaController {
         model.addAttribute("persona", new Persona());
         model.addAttribute("paises", listaPaises);
         return "crear";
+    }
+    
+    @GetMapping("/delete/(id)")
+    public String eliminarPersona(@PathVariable("id")Long idPersona){
+        personaService.delete(idPersona);
+        return "redirect:/persona";
+    }
+    
+    @PostMapping("/save")
+    public String guardarPersona(@ModelAttribute Persona persona){
+        personaService.savePersona(persona);
+        return "redirect;/persona";
+    }
+    
+    @GetMapping("/editPersona/(id)")
+    public String editarPersona(@PathVariable("id")Long idPersona, Model model){
+    Persona persona = personaService.getPersonaById(idPersona);
+    List<Pais> listaPaises = paisService.listCountry();
+    model.addAttribute("persona", persona);
+    model.addAttribute("paises", listaPaises);
+    return "crear";
     }
 }
     
